2a18603a04e5ff025581416b76f62d914d4c972b
EnigmaticAbyss <65163914+EnigmaticAbyss@users.noreply.github.com>
2021-12-18 18:58:03 +0330

    Delete RIscVCPU/Unused stuffs for pc directory

--- RIscVCPU/Unused stuffs for pc/DFlipFlop/d_flip_flop.v
+++
@@ -1,6 +0,0 @@
-module d_flip_flop(input D, input clk, output reg Q);
-    always @(posedge clk) begin
-        Q <= D;
-    end
-endmodule
-// d_flip_flop_tb
--- RIscVCPU/Unused stuffs for pc/clock/clock_maker.v
+++
@@ -1,4 +0,0 @@
-module clock_generator (output reg clk);
-    initial clk <= 0;
-    always #5 clk <= (!clk);
-endmodule    
